fc0872069dc67a45d2c5b67e97b58b5df92bd35b
Sakar Masoud <masoudsakar@gmail.com>
2022-10-31 10:12:12 +0300

    Sakar Masoud

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(
+    (resp) => resp.json(),
+    )
+  .then((json) =>{
+    renderBooks(json)
+    console.log(json)
+  } );
 }
 
 function renderBooks(books) {
